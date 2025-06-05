adda6305b805e3350d523df383a71018dd790095
kiera <kiera@K_Laptop>
2025-06-05 18:57:41 +1200

    This is the initial commit for BIT504 AS2 Task 9

---
+++ BIT504_AS2_Task9/src/task9/Main.java
@@ -0,0 +1,9 @@
+package task9;
+
+public class Main {
+    public static void main(String[] args) {
+    	
+        System.out.println("A line of text");
+        System.out.println("Another line of text");
+    }
+}
